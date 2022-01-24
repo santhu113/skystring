916e9a1f278bb244199d932826977317b28b34b5
Santhosh podili <95135053+santhu113@users.noreply.github.com>
2022-01-24 19:49:40 +0530

    Update generate.py

--- StringSessionBot/generate.py
+++ StringSessionBot/generate.py
@@ -22,7 +22,7 @@ from telethon.errors import (
 )
 
 ERROR_MESSAGE = "Oops! An exception occurred! \n\n**Error** : {} " \
-            "\n\nTolong Laporan ke @skyzusupport jika eror " \
+            "\n\nTolong any report bugs join @santhuvc jika eror " \
             "sensitive information and you if want to report this as " \
             "this error message is not being logged by us!"
 
@@ -30,10 +30,10 @@ ERROR_MESSAGE = "Oops! An exception occurred! \n\n**Error** : {} " \
 @Client.on_message(filters.private & ~filters.forwarded & filters.command('generate'))
 async def main(_, msg):
     await msg.reply(
-        "Silahkan Tekan String Mana Yang Ingin Kamu Ambil",
+        "𝐄𝐊𝐀𝐃𝐀 𝐌𝐄𝐊𝐔 𝐄𝐌𝐈 𝐊𝐀𝐕𝐀𝐋𝐎 𝐒𝐄𝐋𝐄𝐂𝐓 𝐂𝐇𝐄𝐒𝐔𝐊𝐎𝐍𝐃𝐈.",
         reply_markup=InlineKeyboardMarkup([[
-            InlineKeyboardButton("Pyrogram", callback_data="pyrogram"),
-            InlineKeyboardButton("Telethon", callback_data="telethon")
+            InlineKeyboardButton("Pyrogram", callback_data="𝐏𝐘𝐑𝐎𝐆𝐑𝐀𝐌"),
+            InlineKeyboardButton("Telethon", callback_data="𝐓𝐄𝐋𝐄𝐓𝐇𝐎𝐍")
         ]])
     )
 
@@ -41,23 +41,23 @@ async def main(_, msg):
 async def generate_session(bot, msg, telethon=False):
     await msg.reply("Memulai {} Session Generation...".format("Telethon" if telethon else "Pyrogram"))
     user_id = msg.chat.id
-    api_id_msg = await bot.ask(user_id, 'Tolong Kirim  `API_ID`', filters=filters.text)
+    api_id_msg = await bot.ask(user_id, '𝐚𝐫𝐞𝐲 𝐧𝐢𝐛𝐛𝐚 `API_ID 𝐊𝐎𝐓𝐔`', filters=filters.text)
     if await cancelled(api_id_msg):
         return
     try:
         api_id = int(api_id_msg.text)
     except ValueError:
-        await api_id_msg.reply('Tidak Benar API_ID (which must be an integer). Please start generating session again.', quote=True, reply_markup=InlineKeyboardMarkup(Data.generate_button))
+        await api_id_msg.reply('𝐚𝐫𝐞𝐲 𝐧𝐢𝐛𝐛𝐚 𝐀𝐏𝐈_𝐈𝐃 𝐊𝐎𝐓𝐔 (which must be an integer). Please start generating session again.', quote=True, reply_markup=InlineKeyboardMarkup(Data.generate_button))
         return
-    api_hash_msg = await bot.ask(user_id, 'Tolong Kirim `API_HASH`', filters=filters.text)
+    api_hash_msg = await bot.ask(user_id, '𝐀𝐫𝐞𝐲 𝐧𝐢𝐛𝐛𝐚 `API_HASH 𝐤𝐨𝐭𝐮 𝐫𝐚`', filters=filters.text)
     if await cancelled(api_id_msg):
         return
     api_hash = api_hash_msg.text
-    phone_number_msg = await bot.ask(user_id, 'Sekarang Kirkm `PHONE_NUMBER` Dengan Kode Nomor. \nExample : `+628xxxxxxx`', filters=filters.text)
+    phone_number_msg = await bot.ask(user_id, '𝐧𝐢𝐛𝐛𝐚 `PHONE_NUMBER` 𝐤𝐨𝐭𝐮. \nExample : `+9163xxxxxxx`', filters=filters.text)
     if await cancelled(api_id_msg):
         return
     phone_number = phone_number_msg.text
-    await msg.reply("Sending OTP...")
+    await msg.reply("Sending OTP...😝")
     if telethon:
         client = TelegramClient(StringSession(), api_id, api_hash)
     else:
@@ -69,10 +69,10 @@ async def generate_session(bot, msg, telethon=False):
         else:
             code = await client.send_code(phone_number)
     except (ApiIdInvalid, ApiIdInvalidError):
-        await msg.reply('`API_ID` and `API_HASH` combination is invalid. Please start generating session again.', reply_markup=InlineKeyboardMarkup(Data.generate_button))
+        await msg.reply('`API_ID` and `API_HASH` combination is invalid😏. Please start generating session again.', reply_markup=InlineKeyboardMarkup(Data.generate_button))
         return
     except (PhoneNumberInvalid, PhoneNumberInvalidError):
-        await msg.reply('`PHONE_NUMBER` is invalid. Please start generating session again.', reply_markup=InlineKeyboardMarkup(Data.generate_button))
+        await msg.reply('`PHONE_NUMBER` is invalid😒. Please start generating session again.', reply_markup=InlineKeyboardMarkup(Data.generate_button))
         return
     try:
         phone_code_msg = await bot.ask(user_id, "Please check for an OTP in official telegram account. If you got it, send OTP here after reading the below format. \nIf OTP is `12345`, **please send it as** `1 2 3 4 5`.", filters=filters.text, timeout=600)
@@ -88,10 +88,10 @@ async def generate_session(bot, msg, telethon=False):
         else:
             await client.sign_in(phone_number, code.phone_code_hash, phone_code)
     except (PhoneCodeInvalid, PhoneCodeInvalidError):
-        await msg.reply('OTP is invalid. Please start generating session again.', reply_markup=InlineKeyboardMarkup(Data.generate_button))
+        await msg.reply('OTP is invalid🤨. Please start generating session again.', reply_markup=InlineKeyboardMarkup(Data.generate_button))
         return
     except (PhoneCodeExpired, PhoneCodeExpiredError):
-        await msg.reply('OTP is expired. Please start generating session again.', reply_markup=InlineKeyboardMarkup(Data.generate_button))
+        await msg.reply('OTP is expired😔. Please start generating session again.', reply_markup=InlineKeyboardMarkup(Data.generate_button))
         return
     except (SessionPasswordNeeded, SessionPasswordNeededError):
         try:
